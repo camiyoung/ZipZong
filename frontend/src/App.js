07d024374d8fec3dbd763ca240b4f692fb31c105
박종민 <qooktree@naver.com>
2022-07-18 17:02:02 +0900

    Feat: 버튼 추가 [Jira S07P12A805-27]

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,14 @@
 import './App.css'
-import Button from './components/Button'
+import Button from './components/button/Button'
+import Select from './components/input/Select'
+import Text from './components/input/Text'
 
 function App() {
   return (
     <div className="App">
-        <Button />
+        <Button btnname="이거 들어갔나" />
+        <Select />
+        <Text labelInput="1" name="제목" />
     </div>
   )
 }
