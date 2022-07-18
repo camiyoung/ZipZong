41b8fc4416c870b03911ed87b6408a7e9fdc65c0
Songji <youthful_15@naver.com>
2022-07-18 17:52:52 +0900

    Feat: Icon component 추가 [S07P12A805-48]
    Icon component 추가

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,12 @@
 import './App.css'
 import Button from './components/Button'
+import Icon from './components/Icon'
 
 function App() {
   return (
     <div className="App">
         <Button />
+        <Icon image="https://news.samsungdisplay.com/wp-content/uploads/2022/05/IT_twi001t1345955-1-1024x639.jpg"/>
     </div>
   )
 }
