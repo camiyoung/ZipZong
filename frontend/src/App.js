fa9fd595f65eae58e125e26e1701dbfbac340d41
박종민 <qooktree@naver.com>
2022-07-18 17:26:07 +0900

    Remove: App.css 삭제, Feat: 버튼 컴포넌트 추가, Input 컴포넌트 추가중

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,14 +1,19 @@
-import './App.css'
 import Button from './components/button/Button'
 import Select from './components/input/Select'
 import Text from './components/input/Text'
+import SmallTextInput from './components/input/SmallTextInput'
+import LargeTextInput from './components/input/LargeTextInput'
+import TextArea from './components/input/TextArea'
 
 function App() {
   return (
-    <div className="App">
+    <div>
         <Button btnname="이거 들어갔나" />
         <Select />
         <Text labelInput="1" name="제목" />
+        <SmallTextInput inputName="제목(small)" />
+        <LargeTextInput inputName="제목(large)" />
+        <TextArea TextAreaName="제목(textarea)" />
     </div>
   )
 }
---
+++ frontend/src/components/input/TextArea.jsx
@@ -0,0 +1,9 @@
+export default function TextArea(props){
+
+  return (
+    <div>
+      <label for="message" class="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-400">{props.textAreaName}</label>
+      <textarea id="message" rows="4" class="block p-2.5 w-96 text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Leave a comment..."></textarea>
+    </div>
+  )
+}
