378e30657216c1f69df09e300c38610db21be901
박종민 <qooktree@naver.com>
2022-07-18 16:10:34 +0900

    Remove: logo, test   Fix: TailwindCSS

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,28 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.css'
 import Button from './components/Button'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
         <Button />
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      <p className="text-base">asd</p>
-      </header>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
