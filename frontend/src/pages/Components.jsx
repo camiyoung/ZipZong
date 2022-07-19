0ad1bb55aee745649c138f706c8c828ad98471de
camiyoung <anji6978@naver.com>
2022-07-19 13:12:12 +0900

    Create: components페이지 생성

--- frontend/src/pages/Components.jsx
+++ frontend/src/pages/Components.jsx
@@ -6,17 +6,27 @@ import TextArea from "../components/input/TextArea"
 import Icon from "../components/Icon"
 export default function Components() {
   return (
-    <div>
+    <div className="prose prose-slate">
+      <h1> 컴포넌트 목록 </h1>
+      <h2>Buttons</h2>
       <Button btnname="이거 들어갔나" />
+      <h2>Form 요소</h2>
       <Select
         selectName="제목(select)"
         options="옵션의 value들"
         optionName="출력되는 값들"
       />
+
       <SmallTextInput inputName="제목(small)" />
       <LargeTextInput inputName="제목(large)" />
       <TextArea TextAreaName="제목(textarea)" />
+      <h2>Image Placeholder</h2>
       <Icon image="https://news.samsungdisplay.com/wp-content/uploads/2022/05/IT_twi001t1345955-1-1024x639.jpg" />
+
+      <h2>Card</h2>
+      <h2>List</h2>
+      <h2>Name Square</h2>
+      <h2>Navbar</h2>
     </div>
   )
 }
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -3,10 +3,12 @@ import { Link } from "react-router-dom"
 export default function Home() {
   return (
     <div>
-      main page
-      <button className="bg-yellow-400">
-        <Link to="/components">컴포넌트 보기</Link>
-      </button>
+      <h1>메인페이지 </h1>
+      <Link to="/components">
+        <button className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">
+          컴포넌트 보기
+        </button>
+      </Link>
     </div>
   )
 }
