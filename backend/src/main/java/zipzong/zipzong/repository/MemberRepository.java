2d3991c46deadb266c91287c0c9b8cb7750fa56f
chu1214 <las1260@naver.com>
2022-07-20 21:45:27 +0900

    Feat : 전역 예외 처리 + 커스텀 예외 생성 가능

--- backend/src/main/java/zipzong/zipzong/repository/MemberRepository.java
+++ backend/src/main/java/zipzong/zipzong/repository/MemberRepository.java
@@ -8,4 +8,5 @@ import java.util.Optional;
 
 public interface MemberRepository extends JpaRepository<Member, Long> {
     Optional<Member> findByEmailAndProvider(String email, String provider);
+    Optional<Member> findByNickname(String nickname);
 }
