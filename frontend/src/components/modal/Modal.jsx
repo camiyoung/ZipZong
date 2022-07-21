fa328f8a30d7bf5e0c1c8cde3bb3b81d13cb97d6
Songji <youthful_15@naver.com>
2022-07-21 10:04:44 +0900

    Feat: modal 바깥 클릭시 종료 기능 추가

--- frontend/src/components/modal/Modal.jsx
+++ frontend/src/components/modal/Modal.jsx
@@ -1,10 +1,8 @@
-import React, { useRef } from "react"
+import React, { useRef } from "react";
 
 export default function Modal({ isOpen, modalClose, children }) {
-  const modalEl = useRef()
-  const showHide = isOpen ? "visible relative z-10" : "invisible relative z-10"
-
-  console.log(modalEl.current)
+  const outModal = useRef();
+  const showHide = isOpen ? "visible relative z-10" : "invisible relative z-10";
 
   return (
     <div
@@ -12,11 +10,20 @@ export default function Modal({ isOpen, modalClose, children }) {
       aria-labelledby="modal-title"
       role="dialog"
       aria-modal="true"
-      ref={modalEl}
     >
       <div class="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity"></div>
-      <div class="fixed z-10 inset-0 overflow-y-auto">
-        <div class="flex items-end sm:items-center justify-center min-h-full p-4 text-center sm:p-0">
+      <div
+        class="fixed z-10 inset-0 overflow-y-auto"
+        onClick={(event) => {
+          if (event.target === outModal.current) {
+            modalClose();
+          }
+        }}
+      >
+        <div
+          class="flex items-end sm:items-center justify-center min-h-full p-4 text-center sm:p-0"
+          ref={outModal}
+        >
           <div class="relative bg-white rounded-lg text-left overflow-hidden shadow-xl transform transition-all sm:my-8 sm:max-w-lg sm:w-full">
             <div class="bg-white px-4 pt-5 pb-4 sm:p-6 sm:pb-4">
               <div class="mt-3 text-center sm:mt-0 sm:ml-4 sm:text-left">
@@ -27,5 +34,5 @@ export default function Modal({ isOpen, modalClose, children }) {
         </div>
       </div>
     </div>
-  )
+  );
 }
