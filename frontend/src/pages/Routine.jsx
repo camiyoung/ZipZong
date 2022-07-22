1afa2236fb0f34e97bb75632a77c541a57c52050
songji <youthful_15@naver.com>
2022-07-23 02:55:22 +0900

    Feat: Routine 페이지 작성

--- frontend/src/pages/Routine.jsx
+++ frontend/src/pages/Routine.jsx
@@ -1,7 +1,14 @@
+import RoutineList from "../features/routine/RoutineList"
+
 export default function Routine() {
   return (
     <div>
-      <div className="flex justify-center">그룹 루틴</div>
+      <div className="flex justify-center p-5 pt-10 text-primary-600 text-3xl">
+        그룹 루틴
+      </div>
+      <div className="flex justify-center">
+        <RoutineList />
+      </div>
     </div>
   )
 }
