f756f3216291a89ad4dc657d9f878edb896a2b99
qooktree1 <qooktree@gmail.com>
2022-07-22 15:32:21 +0900

    Fix: 토큰 수정

--- frontend/src/features/login/LoginRedirect.jsx
+++ frontend/src/features/login/LoginRedirect.jsx
@@ -1,8 +1,13 @@
 export default function LoginRedirect(){
   const token = new URL(window.location.href).searchParams.get("token")
+  const refreshToken = new URL(window.location.href).searchParams.get("refreshToken")
+  const accessTokenExpiration = new URL(window.location.href).searchParams.get("accessTokenExpiration")
+  const refreshTokenExpiration = new URL(window.location.href).searchParams.get("refreshTokenExpiration")
   if (token) {
     localStorage.setItem("token", token)
-    console.log(token)
+    localStorage.setItem("refreshToken", refreshToken)
+    localStorage.setItem("accessTokenExpiration", accessTokenExpiration)
+    localStorage.setItem("refreshTokenExpiration", refreshTokenExpiration)
   }
   window.location.replace('/login')
 }
