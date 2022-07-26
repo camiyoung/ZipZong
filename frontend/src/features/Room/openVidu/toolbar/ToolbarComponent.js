9501d02aa7c0e9d141aa6995a6113ee290d01895
안지영 <anji6978@naver.com>
2022-07-26 13:10:59 +0900

    Feat: openvidu toolbar 추가[S07P12A805-102]

--- frontend/src/features/Room/openVidu/toolbar/ToolbarComponent.js
+++ frontend/src/features/Room/openVidu/toolbar/ToolbarComponent.js
@@ -1,26 +1,14 @@
 import React, { Component } from "react"
 import "./ToolbarComponent.css"
-
-// import AppBar from "@material-ui/core/AppBar"
-// import Toolbar from "@material-ui/core/Toolbar"
-
-// import Mic from "@material-ui/icons/Mic"
-// import MicOff from "@material-ui/icons/MicOff"
-// import Videocam from "@material-ui/icons/Videocam"
-// import VideocamOff from "@material-ui/icons/VideocamOff"
-// import Fullscreen from "@material-ui/icons/Fullscreen"
-// import FullscreenExit from "@material-ui/icons/FullscreenExit"
-// import SwitchVideoIcon from "@material-ui/icons/SwitchVideo"
-// import PictureInPicture from "@material-ui/icons/PictureInPicture"
-// import ScreenShare from "@material-ui/icons/ScreenShare"
-// import StopScreenShare from "@material-ui/icons/StopScreenShare"
-// import Tooltip from "@material-ui/core/Tooltip"
-// import PowerSettingsNew from "@material-ui/icons/PowerSettingsNew"
-// import QuestionAnswer from "@material-ui/icons/QuestionAnswer"
-
-// import IconButton from "@material-ui/core/IconButton"
-
-// const logo = require("../../assets/images/openvidu_logo.png")
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import {
+  faMicrophone,
+  faMicrophoneSlash,
+  faVideo,
+  faVideoSlash,
+  faCameraRotate,
+  faArrowRightFromBracket,
+} from "@fortawesome/free-solid-svg-icons"
 
 export default class ToolbarComponent extends Component {
   constructor(props) {
@@ -73,88 +61,72 @@ export default class ToolbarComponent extends Component {
     const mySessionId = this.props.sessionId
     const localUser = this.props.user
     return (
-      <div>툴바</div>
-      // <AppBar className="toolbar" id="header">
-      //   <Toolbar className="toolbar">
-      //     <div id="navSessionInfo">
-      //       <img id="header_img" alt="OpenVidu Logo" src={logo} />
+      <div className=" flex">
+        <div>
+          {this.props.sessionId && (
+            <div>
+              <span id="session-title">{mySessionId}</span>
+            </div>
+          )}
+        </div>
 
-      //       {this.props.sessionId && (
-      //         <div id="titleContent">
-      //           <span id="session-title">{mySessionId}</span>
-      //         </div>
-      //       )}
-      //     </div>
+        <div className=" flex">
+          <div
+            color="inherit"
+            className="navButton"
+            id="navMicButton"
+            onClick={this.micStatusChanged}
+          >
+            {localUser !== undefined && localUser.isAudioActive() ? (
+              <FontAwesomeIcon icon={faMicrophone} />
+            ) : (
+              <FontAwesomeIcon icon={faMicrophoneSlash} />
+            )}
+          </div>
 
-      //     <div className="buttonsContent">
-      //       <IconButton
-      //         color="inherit"
-      //         className="navButton"
-      //         id="navMicButton"
-      //         onClick={this.micStatusChanged}
-      //       >
-      //         {localUser !== undefined && localUser.isAudioActive() ? (
-      //           <Mic />
-      //         ) : (
-      //           <MicOff color="secondary" />
-      //         )}
-      //       </IconButton>
+          <div
+            color="inherit"
+            className="navButton"
+            id="navCamButton"
+            onClick={this.camStatusChanged}
+          >
+            {localUser !== undefined && localUser.isVideoActive() ? (
+              <FontAwesomeIcon icon={faVideo} />
+            ) : (
+              <FontAwesomeIcon icon={faVideoSlash} />
+            )}
+          </div>
 
-      //       <IconButton
-      //         color="inherit"
-      //         className="navButton"
-      //         id="navCamButton"
-      //         onClick={this.camStatusChanged}
-      //       >
-      //         {localUser !== undefined && localUser.isVideoActive() ? (
-      //           <Videocam />
-      //         ) : (
-      //           <VideocamOff color="secondary" />
-      //         )}
-      //       </IconButton>
-
-      //       {/* <IconButton color="inherit" className="navButton" onClick={this.screenShare}>
-      //                       {localUser !== undefined && localUser.isScreenShareActive() ? <PictureInPicture /> : <ScreenShare />}
-      //                   </IconButton> */}
-      //       {/*
-      //                   {localUser !== undefined &&
-      //                       localUser.isScreenShareActive() && (
-      //                           <IconButton onClick={this.stopScreenShare} id="navScreenButton">
-      //                               <StopScreenShare color="secondary" />
-      //                           </IconButton>
-      //                       )} */}
-
-      //       <IconButton
-      //         color="inherit"
-      //         className="navButton"
-      //         onClick={this.switchCamera}
-      //       >
-      //         <SwitchVideoIcon />
-      //       </IconButton>
-      //       {/* <IconButton color="inherit" className="navButton" onClick={this.toggleFullscreen}>
-      //                       {localUser !== undefined && this.state.fullscreen ? <FullscreenExit /> : <Fullscreen />}
-      //                   </IconButton> */}
-      //       <IconButton
-      //         color="secondary"
-      //         className="navButton"
-      //         onClick={this.leaveSession}
-      //         id="navLeaveButton"
-      //       >
-      //         <PowerSettingsNew />
-      //       </IconButton>
-      //       <IconButton
-      //         color="inherit"
-      //         onClick={this.toggleChat}
-      //         id="navChatButton"
-      //       >
-      //         {this.props.showNotification && <div id="point" className="" />}
-      //         <Tooltip title="Chat">
-      //           <QuestionAnswer />
-      //         </Tooltip>
-      //       </IconButton>
-      //     </div>
-      //   </Toolbar>
-      // </AppBar>
+          <div
+            color="inherit"
+            className="navButton"
+            onClick={this.switchCamera}
+          >
+            <FontAwesomeIcon icon={faCameraRotate} />
+          </div>
+          {/* <div color="inherit" className="navButton" onClick={this.toggleFullscreen}>
+                            {localUser !== undefined && this.state.fullscreen ? <FullscreenExit /> : <Fullscreen />}
+                        </div> */}
+          <div
+            color="secondary"
+            className="navButton"
+            onClick={this.leaveSession}
+            id="navLeaveButton"
+          >
+            <FontAwesomeIcon icon={faArrowRightFromBracket} />
+          </div>
+          {/* <div
+              color="inherit"
+              onClick={this.toggleChat}
+              id="navChatButton"
+            >
+              {this.props.showNotification && <div id="point" className="" />}
+              <Tooltip title="Chat">
+                <QuestionAnswer />
+              </Tooltip>
+            </div> */}
+        </div>
+      </div>
     )
   }
 }
