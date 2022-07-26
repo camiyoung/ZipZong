e68bb42b3781af84d7b5d22491144322483bc176
박종민 <qooktree@naver.com>
2022-07-26 18:10:14 +0900

    Feat: Login, MyPage 관련 상태관리 진행중

--- frontend/src/features/login/memberReducer.js
+++ frontend/src/features/login/memberReducer.js
@@ -2,6 +2,10 @@ import { createSlice } from "@reduxjs/toolkit"
 
 const initialState = {
   memberId: null,
+  memberName: null,
+  memberEmail: null,
+  memberProvider: null,
+  memberNickname: null,
 }
 
 export const memberSlice = createSlice({
@@ -11,9 +15,27 @@ export const memberSlice = createSlice({
     checkMemberId: (state, action) => {
       state.value = action.payload
     },
+    checkMemberName: (state, action) => {
+      state.value = action.payload
+    },
+    checkMemberEmail: (state, action) => {
+      state.value = action.payload
+    },
+    checkMemberProvider: (state, action) => {
+      state.value = action.payload
+    },
+    checkMemberNickname: (state, action) => {
+      state.value = action.payload
+    },
   },
 })
 
-export const { checkMemberId } = memberSlice.actions
+export const {
+  checkMemberId,
+  checkMemberName,
+  checkMemberEmail,
+  checkMemberProvider,
+  checkMemberNickname,
+} = memberSlice.actions
 
 export default memberSlice.reducer
