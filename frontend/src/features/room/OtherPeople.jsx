25b2f21a7dd75397a93d58cc3158056ae248e1ec
안지영 <anji6978@naver.com>
2022-07-27 17:08:01 +0900

    Design: 운동방 UI 변경[S07P12A805-102]

--- frontend/src/features/room/OtherPeople.jsx
+++ frontend/src/features/room/OtherPeople.jsx
@@ -4,7 +4,7 @@ import StreamComponent from "./openVidu/stream/StreamComponent"
 const tmp = ["유저1", "유저2", "유저3", "유저4"]
 const Video = ({ user, streamId }) => {
   return (
-    <div className="border w-1/6 min-w-[200px] bg-white rounded-xl">
+    <div className="w-full  bg-white  my-2">
       <StreamComponent user={user} streamId={streamId} />
     </div>
   )
@@ -12,7 +12,7 @@ const Video = ({ user, streamId }) => {
 
 export default function OtherPeople({ subscribers }) {
   return (
-    <div className=" h-1/6 flex w-full justify-between p-2 bg-lgBlue-400">
+    <div className=" h-full w-1/6 flex flex-col items-center justify-center  pl-5 ">
       {subscribers.map((sub, i) => (
         <Video
           user={sub}
--- frontend/src/pages/RoomPage.jsx
+++ frontend/src/pages/RoomPage.jsx
@@ -2,5 +2,9 @@ import React from "react"
 import Room from "../features/room/Room"
 
 export default function RoomPage() {
-  return <Room />
+  return (
+    <div className="bg-gradient-to-r from-lgBlue-500 to-primary-600 p-4 h-screen w-screen">
+      <Room />
+    </div>
+  )
 }
