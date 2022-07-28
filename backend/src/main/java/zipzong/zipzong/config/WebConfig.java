7f4dcc8a0fb4d491b16232580a852e9edd5b25a6
junuu <bababrll@naver.com>
2022-07-28 16:52:10 +0900

    feat: CORS 설정

---
+++ backend/src/main/java/zipzong/zipzong/config/WebConfig.java
@@ -0,0 +1,20 @@
+package zipzong.zipzong.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@EnableWebMvc
+public class WebConfig implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:3000")
+                .allowedMethods("*")
+                .allowCredentials(false)
+                .maxAge(3000);
+
+    }
+}
