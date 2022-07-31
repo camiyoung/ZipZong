46ab3e924407c4d890f4cc94e61a8cde55281afb
camiyoung <anji6978@naver.com>
2022-07-31 17:24:10 +0900

    Feat: 동작 인식 skeleton 띄우기 [S07P12A805-97]

--- frontend/src/features/room/teachableMachine/TeachableMachine.jsx
+++ frontend/src/features/room/teachableMachine/TeachableMachine.jsx
@@ -16,12 +16,18 @@ export default class TeachableMachine extends Component {
     this.state = {
       resEle: undefined,
       successCount: 0,
+      ctx: undefined,
     }
     this.beforAction = undefined
+    this.videoRef = undefined
+    this.canvasRef = React.createRef()
+    this.canvas = undefined
   }
 
   componentDidMount() {
-    console.log("비디오 프롭", this.props.video)
+    this.videoRef = this.props.myVideoRef
+    console.log("비디오 ", this.videoRef.current)
+
     this.makeModel()
   }
 
@@ -30,7 +36,18 @@ export default class TeachableMachine extends Component {
     this.maxPredictions = this.model.getTotalClasses()
     console.log("최대 예측값:", this.maxPredictions)
     console.log("모델:", this.model)
+    await this.init()
     this.loop()
+  }
+
+  async init() {
+    this.videoRef.current.width = this.videoRef.current.offsetWidth
+    this.videoRef.current.height = this.videoRef.current.offsetHeight
+    this.canvas = this.canvasRef.current
+    console.log("캔버스", this.canvas)
+    this.canvas.width = this.videoRef.current.width
+    this.canvas.height = this.videoRef.current.height
+    this.setState({ ctx: this.canvas.getContext("2d") })
   }
 
   async loop() {
@@ -39,9 +56,13 @@ export default class TeachableMachine extends Component {
   }
 
   async predict() {
+    // console.log("REF:", this.videoRef)
+
+    // const video = this.videoRef.current
     const { pose, posenetOutput } = await this.model.estimatePose(
-      this.props.video
+      this.videoRef.current
     )
+    // console.log(pose)
     const prediction = await this.model.predict(posenetOutput)
     const predictionsList = prediction.map((res, i) => (
       <div key={i}>
@@ -49,6 +70,7 @@ export default class TeachableMachine extends Component {
       </div>
     ))
     // console.log("리스트", predictionsList)
+    this.drawPose(pose)
     this.setState({ resEle: predictionsList })
     this.updateCount(prediction)
   }
@@ -74,13 +96,39 @@ export default class TeachableMachine extends Component {
     })
   }
 
+  drawPose(pose) {
+    if (pose) {
+      this.state.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height)
+      const minPartConfidence = 0.5
+      tmPose.drawKeypoints(pose.keypoints, minPartConfidence, this.state.ctx)
+      tmPose.drawSkeleton(pose.keypoints, minPartConfidence, this.state.ctx)
+    }
+  }
+
   render() {
     return (
       <div className="w-full h-full absolute z-20">
-        <div className="w-[200px] h-[400px] bg-white absolute right-0 border-4">
-          {this.state.resEle}
-          count: {this.state.successCount}
+        <div className="w-[440px] h-[200px] bg-white absolute right-[-280px] border-4">
+          <div className="font-semibold">
+            <p> 1. 티처블 머신이 로딩되기까지 약 10s의 시간이 소요됩니다.</p>
+            <p>
+              2. 로딩이후 화면의 크기를 변경하면 skeleton 위치가 어긋납니다.
+            </p>
+            <p> 3. 로딩 이후 카메라를 switch하면 안됩니다!!</p>
+            <p></p>
+          </div>
+          {this.state.resEle ? (
+            <div className="border-2 border-mainBlue">
+              {this.state.resEle}
+              count: {this.state.successCount}
+            </div>
+          ) : (
+            <div className="font-bold text-primary-500 text-2xl">
+              Teachable Machine 로딩중{" "}
+            </div>
+          )}
         </div>
+        <canvas ref={this.canvasRef} className="z-50 -scale-x-[1]"></canvas>
       </div>
     )
   }
