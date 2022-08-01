aa30946aba11c5f58394ec5dc9c209c1e00e2f77
박종민 <qooktree@naver.com>
2022-08-01 11:06:03 +0900

    Chore: react-persist 추가

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -1,12 +1,24 @@
-import { configureStore } from "@reduxjs/toolkit"
+import { combineReducers, configureStore } from "@reduxjs/toolkit"
+import { persistReducer } from "redux-persist"
+import storage from "redux-persist/lib/storage"
 import memberReducer from "./features/login/memberReducer"
 import myPageReducer from "./features/myPage/myPageReducer"
 import routineReducer from "./features/routine/routineReducer"
+import thunk from "redux-thunk"
+
+const reducers = combineReducers({
+  member: memberReducer,
+  mypage: myPageReducer,
+  routine: routineReducer,
+})
+
+const persistConfig = {
+  key: "root",
+  storage,
+}
+
+const persistedReducer = persistReducer(persistConfig, reducers)
 
 export const store = configureStore({
-  reducer: {
-    member: memberReducer,
-    mypage: myPageReducer,
-    routine: routineReducer,
-  },
+  reducer: persistedReducer,
 })
