53a45dca080f6b2d078ec9dde62c534f53751cb0
camiyoung <anji6978@naver.com>
2022-08-02 20:25:15 +0900

    Feat: 타이머 컴포넌트 생[S07P12A805-96]

--- frontend/src/features/room/workout/WorkOut.jsx
+++ frontend/src/features/room/workout/WorkOut.jsx
@@ -1,13 +1,47 @@
-import React from "react"
+import React, { useEffect, useState } from "react"
 import TeachableMachine from "../teachableMachine/TeachableMachine"
 import Timer from "./Timer"
 
-const type = "breaktime"
+const exersiceRoutine = {
+  routineId: 2,
+  routineName: "tmproutine",
+  exercise: [
+    { name: "LEGRAISE", count: 5 },
+    { name: "PUSHUP", count: 5 },
+    { name: "BURPEE", count: 5 },
+    { name: "LEGRAISE", count: 5 },
+    { name: "PUSHUP", count: 5 },
+    { name: "BURPEE", count: 5 },
+  ],
+  breaktime: 3,
+}
+// 운동시간 5초
+
 const WorkOut = ({ myVideo }) => {
+  const { breaktime, exercise: exerciseInfos } = exersiceRoutine
+  let routine = []
+
+  useEffect(() => {
+    exerciseInfos.forEach((info) => {
+      routine.push({ type: "exercise", duration: 5, name: info.name })
+      routine.push({ type: "breaktime", duration: breaktime })
+    })
+    routine.pop()
+  }, [])
+
+  const [currentAction, SetCurrentAction] = useState({
+    type: "ready",
+    duration: 5,
+  })
+
+  const changeExerciseState = () => {
+    routine.forEach((current) => {})
+  }
+
   return (
     <div className="w-full h-full absolute">
       {/* <TeachableMachine myVideoRef={myVideo.props.myVideoRef} /> */}
-      <Timer type={type} duration={10} />
+      <Timer type={currentAction.type} duration={currentAction.duration} />
     </div>
   )
 }
