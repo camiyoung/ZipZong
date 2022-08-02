9f37dcf22c37ef6444ad8b38b3d70b59fd4bf813
seulgi <seulgi9834@naver.com>
2022-08-03 03:24:08 +0900

    Fix: 루틴 수정 응답 고침[S07P12A805-153]
    루틴 아이디 반환에서 그룹 아이디 반환으로 변경했습니다.

--- backend/src/main/java/zipzong/zipzong/api/controller/RoutineController.java
+++ backend/src/main/java/zipzong/zipzong/api/controller/RoutineController.java
@@ -42,9 +42,9 @@ public class RoutineController {
 
     //루틴 수정
     @PutMapping("/{routineId}")
-    public ResponseEntity<BasicResponse<Long>> routineModify(@RequestBody RoutineRequest routineRequest,@PathVariable Long routineId) {
-        routineService.updateRoutine(routineId, routineRequest);
-        return new ResponseEntity<>(makeBasicResponse(SUCCESS, routineId), HttpStatus.CREATED);
+    public ResponseEntity<BasicResponse<Long>> routineModify(@RequestBody RoutineRequest routineRequest, @PathVariable Long routineId) {
+        Long teamId = routineService.updateRoutine(routineId, routineRequest);
+        return new ResponseEntity<>(makeBasicResponse(SUCCESS, teamId), HttpStatus.CREATED);
     }
 
     private <T> BasicResponse<T> makeBasicResponse(String message, T data) {
