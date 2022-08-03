d6c2208331ee3b54eb8a2d4f86b92d2f30328103
camiyoung <anji6978@naver.com>
2022-08-03 16:08:32 +0900

    Feat: 운동, 휴식 마다 카운트 초기화 시키기 [S07P12A805-96]

--- frontend/src/features/room/workout/WorkOut.jsx
+++ frontend/src/features/room/workout/WorkOut.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react"
+import React, { useEffect, useRef, useState } from "react"
 import TeachableMachine from "../teachableMachine/TeachableMachine"
 import Timer from "./Timer"
 
@@ -6,44 +6,118 @@ const exersiceRoutine = {
   routineId: 2,
   routineName: "tmproutine",
   exercise: [
-    { name: "LEGRAISE", count: 5 },
-    { name: "PUSHUP", count: 5 },
-    { name: "BURPEE", count: 5 },
-    { name: "LEGRAISE", count: 5 },
-    { name: "PUSHUP", count: 5 },
-    { name: "BURPEE", count: 5 },
+    { name: "LEGRAISE", count: 7 },
+    { name: "PUSHUP", count: 7 },
+    { name: "BURPEE", count: 7 },
+    { name: "LEGRAISE", count: 7 },
+    { name: "PUSHUP", count: 7 },
+    { name: "BURPEE", count: 7 },
   ],
-  breaktime: 3,
+  breaktime: 2,
 }
 // 운동시간 5초
 
+function useTimeout(callback, delay) {
+  const timeoutRef = useRef(null)
+  const savedCallback = useRef(callback)
+  useEffect(() => {
+    savedCallback.current = callback
+  }, [callback])
+  useEffect(() => {
+    const tick = () => savedCallback.current()
+    if (typeof delay === "number") {
+      timeoutRef.current = window.setTimeout(tick, delay)
+      return () => window.clearTimeout(timeoutRef.current)
+    }
+  }, [delay])
+  return timeoutRef
+}
+
 const WorkOut = ({ myVideo }) => {
   const { breaktime, exercise: exerciseInfos } = exersiceRoutine
-  let routine = []
-
-  useEffect(() => {
-    exerciseInfos.forEach((info) => {
-      routine.push({ type: "exercise", duration: 5, name: info.name })
-      routine.push({ type: "breaktime", duration: breaktime })
-    })
-    routine.pop()
-  }, [])
-
-  const [currentAction, SetCurrentAction] = useState({
+  const routine = useRef([])
+  const [currentAction, setCurrentAction] = useState({
     type: "ready",
-    duration: 5,
+    duration: 3,
   })
 
-  const changeExerciseState = () => {
-    routine.forEach((current) => {})
+  const [isRunning, setRunning] = useState(true)
+
+  const routineIdx = useRef(0)
+  useEffect(() => {
+    let todo = []
+    console.log("운동 시작 mounted")
+    exerciseInfos.forEach((info) => {
+      todo.push({ type: "exercise", duration: 3, name: info.name })
+      todo.push({ type: "breaktime", duration: breaktime })
+    })
+    todo.pop()
+
+    routine.current = todo
+    console.log(routine)
+  }, [])
+
+  const changeNextAction = () => {
+    const nextAction = routine.current[routineIdx.current]
+    routineIdx.current++
+    // console.log("다음 동작", nextAction, routineIdx)
+    setCurrentAction(nextAction)
+  }
+  const finishAction = () => {
+    setRunning(false)
   }
 
+  useEffect(() => {
+    if (!isRunning && routineIdx.current < routine.current.length) {
+      // console.log("루틴 인덱스", routineIdx.current)
+      changeNextAction()
+      setRunning(true)
+    } else if (!isRunning && routineIdx.current === routine.current.length) {
+      console.log("운동 루틴 종료!!!!!!")
+    }
+  }, [isRunning])
+
+  useEffect(() => {
+    // console.log("동작 변경")
+  }, [currentAction])
   return (
-    <div className="w-full h-full absolute">
-      {/* <TeachableMachine myVideoRef={myVideo.props.myVideoRef} /> */}
-      <Timer type={currentAction.type} duration={currentAction.duration} />
+    <div>
+      {isRunning && (
+        <Start
+          myVideo={myVideo}
+          action={currentAction}
+          changeAction={changeNextAction}
+          finishAction={finishAction}
+        />
+      )}
     </div>
   )
 }
 
+const Start = ({ myVideo, action, changeAction, finishAction }) => {
+  console.log("현재 동작 정보", action)
+
+  useTimeout(() => {
+    console.log("시간 종료 ")
+    finishAction()
+  }, action.duration * 1000)
+  useEffect(() => {
+    return () => {
+      console.log("현재 동작  Unmount")
+    }
+  }, [])
+  return (
+    <div>
+      <div className="w-full h-full absolute">
+        {/* <TeachableMachine myVideoRef={myVideo.props.myVideoRef} /> */}
+        {<Timer action={action} />}
+        {!!action && (
+          <div className="absolute top-0 bg-white z-50">
+            현재 운동:{action.type}, 시간:{action.duration}
+          </div>
+        )}
+      </div>
+    </div>
+  )
+}
 export default WorkOut
