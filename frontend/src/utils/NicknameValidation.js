573a952e1a80ab5a4882f7b44778767402117363
박종민 <qooktree@naver.com>
2022-08-05 15:35:07 +0900

    Style: console.log 제거

--- frontend/src/utils/NicknameValidation.js
+++ frontend/src/utils/NicknameValidation.js
@@ -2,7 +2,6 @@ import { http } from "../api/axios"
 
 export async function NicknameValidation(nickname) {
   const res = await http.get(`member/duplicate/${nickname}`)
-  console.log("sdf", res)
   if (res.data.data === "NON-DUPLICATE") {
     return res.data.data
   } else if (res.data.data === "DUPLICATE") {
