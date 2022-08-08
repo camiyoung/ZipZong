92013ac573990dd9272d28a1db4607b6d849ec5b
Songji <youthful_15@naver.com>
2022-08-08 16:20:38 +0900

    Desing: routine 페이지 ui 수정

--- frontend/src/features/routine/RoutineList.jsx
+++ frontend/src/features/routine/RoutineList.jsx
@@ -5,7 +5,6 @@ import ImageIcon from "../../components/icon/ImageIcon"
 import Modal from "../../components/modal/Modal"
 import Button from "../../components/button/Button"
 import Card from "../../components/card/Card"
-import { Link } from "react-router-dom"
 import ChangeLanguage from "./ChangeLanguage"
 import { getRoutine, deleteRoutine } from "./routineReducer"
 import { useParams, useNavigate } from "react-router-dom"
@@ -25,7 +24,7 @@ export default function RoutineList() {
   }, [])
 
   return (
-    <div className="flex">
+    <div className="w-4/5">
       <Modal isOpen={isOpen} modalClose={modalClose}>
         <div>
           루틴이 삭제되었습니다.
@@ -34,71 +33,86 @@ export default function RoutineList() {
           </div>
         </div>
       </Modal>
-      {routines.map(({ routineId, routineName, exercise, breakTime }) => {
-        return (
-          <div className="m-3" key={routineId}>
-            <Card size="middle">
-              <div className="">
-                <div className=" bg-lgBlue-500 border-2 m-2 rounded-3xl flex justify-center">
-                  {routineName}
-                </div>
-                {exercise.map(({ name, count }, index) => {
-                  return (
-                    <div className="flex justify-center" key={index}>
-                      <ChangeLanguage exercise={name} />
-                      <span className="ml-1">{count}회</span>
-                    </div>
-                  )
-                })}
-                <div className="flex justify-center">
-                  쉬는 시간 {breakTime}초
-                </div>
-                <div className="flex justify-center p-3">
-                  총 운동 시간 :{" "}
-                  {exercise.length * 60 + (exercise.length - 1) * breakTime} 초
-                </div>
-                <div className="flex justify-center">
-                  <div
-                    className="bg-secondary-300 m-2 w-[50px] rounded-xl flex justify-center text-sm items-center h-[35px] pointer hover:bg-secondary-500"
-                    onClick={() =>
-                      navigate(`/routine/${params.teamId}/${routineId}`)
-                    }
-                  >
-                    수정
+      <div className="w-full flex justify-between">
+        {routines.map(({ routineId, routineName, exercise, breakTime }) => {
+          return (
+            <div className="w-[20%] m-2" key={routineId}>
+              <div className="p-2 bg-white rounded-2xl border border-gray-200 shadow-md">
+                <div className="w-full">
+                  <div className=" bg-lgBlue-400 shadow-sm m-3 p-1 rounded-2xl flex justify-center font-bold text-xl">
+                    {routineName}
                   </div>
-                  <div
-                    className="bg-secondary-300 m-2 w-[50px] rounded-xl flex justify-center text-sm items-center pointer hover:bg-secondary-500"
-                    onClick={() => {
-                      setOpen(true)
-                      dispatch(deleteRoutine(routineId))
-                    }}
-                  >
-                    삭제
+                  <div className="flex flex-col justify-center px-3">
+                    {exercise.map(({ name, count }, index) => {
+                      return (
+                        <div className="flex bg-white rounded-2xl py-1 border-b">
+                          {/* <div className="w-1/6 flex justify-center">
+                          {index + 1}
+                        </div> */}
+                          <div
+                            className="w-4/6 ml-2 flex justify-start items-center"
+                            key={index}
+                          >
+                            <ChangeLanguage exercise={name} />
+                          </div>
+                          <div className="w-2/6 flex mr-3 justify-end">
+                            {count}회
+                          </div>
+                        </div>
+                      )
+                    })}
+                  </div>
+                  <div className="flex justify-center p-3">
+                    쉬는 시간 {breakTime}초
+                  </div>
+                  {/* <div className="flex justify-center">
+                    총 운동 시간 :{" "}
+                    {exercise.length * 60 + (exercise.length - 1) * breakTime}{" "}
+                    초
+                  </div> */}
+                  <div className="flex justify-center mb-1">
+                    <div
+                      className="bg-secondary-300 m-2 w-[50px] rounded-xl flex justify-center text-sm items-center h-[35px] pointer hover:bg-secondary-500"
+                      onClick={() =>
+                        navigate(`/routine/${params.teamId}/${routineId}`)
+                      }
+                    >
+                      수정
+                    </div>
+                    <div
+                      className="bg-secondary-300 m-2 w-[50px] rounded-xl flex justify-center text-sm items-center pointer hover:bg-secondary-500"
+                      onClick={() => {
+                        setOpen(true)
+                        dispatch(deleteRoutine(routineId))
+                      }}
+                    >
+                      삭제
+                    </div>
                   </div>
                 </div>
               </div>
+            </div>
+          )
+        })}
+        {routines.length < 5 ? (
+          <div
+            className="m-3"
+            onClick={() => navigate(`/routine/${params.teamId}/make`)}
+          >
+            <Card size="middle">
+              <div className="flex justify-center p-2">
+                <ImageIcon
+                  image="https://icons-for-free.com/download-icon-circle+more+plus+icon-1320183136549593898_512.png"
+                  size="large"
+                />
+              </div>
+              <p className="p-2 font-bold flex justify-center text-lg">
+                루틴을 추가해보세요.
+              </p>
             </Card>
           </div>
-        )
-      })}
-      {routines.length < 5 ? (
-        <div
-          className="m-3"
-          onClick={() => navigate(`/routine/${params.teamId}/make`)}
-        >
-          <Card size="middle">
-            <div className="flex justify-center p-2">
-              <ImageIcon
-                image="https://icons-for-free.com/download-icon-circle+more+plus+icon-1320183136549593898_512.png"
-                size="large"
-              />
-            </div>
-            <p className="p-2 font-bold flex justify-center text-lg">
-              루틴을 추가해보세요.
-            </p>
-          </Card>
-        </div>
-      ) : null}
+        ) : null}
+      </div>
     </div>
   )
 }
