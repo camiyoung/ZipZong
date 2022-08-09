31e2fef66ff11f9e49f728b27abe739523a276a9
songji <youthful_15@naver.com>
2022-08-10 03:07:39 +0900

    Design: 그룹 설정 그룹 인포 / 획득 아이콘 변경

--- frontend/src/pages/GroupSet.jsx
+++ frontend/src/pages/GroupSet.jsx
@@ -35,11 +35,17 @@ export default function GroupSet() {
   const modalClose = () => setOpen(false)
 
   return (
-    <div className="w-full flex justify-center">
-      <div className="w-4/5 flex flex-col">
-        <GroupSetInfo />
-        <CollectedIcons />
-        <GroupSetMemberList />
+    <div className="w-full flex justify-center mt-5">
+      <div className="w-4/5">
+        <div className="w-full flex">
+          <div className="w-[50%]">
+            <GroupSetInfo />
+            <CollectedIcons />
+          </div>
+          <div className="w-[50%]">
+            <GroupSetMemberList />
+          </div>
+        </div>
         <div className="mx-5 flex justify-end">
           <Button
             text="그룹 삭제"
