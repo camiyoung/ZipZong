282ca59d1469da0c71fbe0d3db8392f2fe70422f
박종민 <qooktree@naver.com>
2022-08-09 16:16:39 +0900

    Fix: 로그인 navigate 코드 수정

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -40,7 +40,7 @@ export default function Login() {
     dispatch(checkMemberId(Number(collectedMemberId)))
 
     //닉네임 중복 확인
-    if (hasNickname && nickname != "") {
+    if (hasNickname && nickname !== "") {
       dispatch(
         nicknamePush({
           memberId: Number(collectedMemberId),
@@ -49,7 +49,7 @@ export default function Login() {
       )
       console.log("닉네임 중복 확인", hasNickname, nickname)
     }
-    return () => navigate("/mypage")
+    navigate("/mypage")
   }, [])
   return (
     <div className="flex">
