2691e85ad8e12f9785b17f187b48dd78e8dcc734
camiyoung <anji6978@naver.com>
2022-08-10 14:20:23 +0900

    Feat: 방폭파(방장 퇴장)시 보여줄 에러 페이지 생성[S07P12A805-241]

---
+++ frontend/src/features/room/ErrorPage.jsx
@@ -0,0 +1,67 @@
+import { useEffect, useState } from "react"
+import { useNavigate } from "react-router"
+
+function ErrorPage({ title = "에러페이지", message = undefined, groupId }) {
+  const navigate = useNavigate()
+  const path = groupId ? groupId : ""
+  const [duration, setDuration] = useState(30)
+
+  useEffect(() => {
+    if (duration === 0) {
+      navigate(`/group/${path}`)
+    }
+    const countdown = setInterval(() => {
+      setDuration((duration) => duration - 1)
+    }, 1000)
+
+    return () => clearInterval(countdown)
+  }, [duration])
+
+  const movePage = () => {
+    navigate(`/group${path}`)
+  }
+  return (
+    <div className="w-full h-full absolute bg-[#000000a1] z-50 top-0 left-0 flex flex-col justify-center items-center">
+      <div className="relative bg-white rounded-lg shadow w-2/6 ">
+        <div className="p-6 text-center">
+          <svg
+            className="mx-auto mb-4 w-14 h-14  text-warning"
+            fill="none"
+            stroke="currentColor"
+            viewBox="0 0 24 24"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth="2"
+              d="M12 8v4m0 4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
+            ></path>
+          </svg>
+          <h3 className="mb-5 text-3xl font-bold text-gray-500 ">{title}</h3>
+          <div className="flex justify-center items-center flex-col mb-7 ">
+            {!!message &&
+              message.map((content, idx) => (
+                <p className="text-gray-500  font-normal" key={idx}>
+                  {content}
+                </p>
+              ))}
+          </div>
+
+          <button
+            type="button"
+            className=" text-darkGray bg-yellow-300 hover:bg-yellow-400 focus:ring-4 focus:outline-none focus:ring-yellow-200 font-medium rounded-lg text-sm inline-flex items-center px-5 py-2.5 text-center mr-2"
+            onClick={movePage}
+          >
+            그룹 페이지로 돌아가기
+          </button>
+          <p className="text-gray-500  font-normal mt-5">
+            {duration}초 후에 자동으로 그룹 페이지로 이동합니다.
+          </p>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default ErrorPage
