4a9d519a49a778627522926c1696d20d8c1ec659
chu1214 <las1260@naver.com>
2022-08-10 13:50:40 +0900

    Fix: 정보 저장 픽스

--- backend/src/main/java/zipzong/zipzong/db/repository/history/TeamHistoryDetailRepository.java
+++ backend/src/main/java/zipzong/zipzong/db/repository/history/TeamHistoryDetailRepository.java
@@ -13,7 +13,7 @@ public interface TeamHistoryDetailRepository extends JpaRepository<TeamHistoryDe
     @Query("select d from TeamHistoryDetail d where d.teamHistory.id = :teamHistoryId")
     List<TeamHistoryDetail> findByTeamHistoryId(@Param("teamHistoryId") Long teamHistoryId);
 
-    @EntityGraph(attributePaths = {"teamHistory, Team"})
+    @EntityGraph(attributePaths = {"teamHistory, team"})
     @Query("select d from TeamHistoryDetail d where d.teamHistory.id in " +
             "(select h from TeamHistory h where h.team.id = :teamId)")
     List<TeamHistoryDetail> teamAllTime(@Param("teamId") Long teamId);
