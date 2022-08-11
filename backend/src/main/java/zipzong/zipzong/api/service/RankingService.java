6436e60cb4f3827386212579ff9076312bd4c254
황승주 <las1260@naver.com>
2022-08-11 11:37:02 +0900

    Fix: redis 비밀번호 설정

--- backend/src/main/java/zipzong/zipzong/api/service/RankingService.java
+++ backend/src/main/java/zipzong/zipzong/api/service/RankingService.java
@@ -45,7 +45,7 @@ public class RankingService {
     private final RedisTemplate<String, String> redisTemplate;
     private static final Long BOUNDARY = 5L;
 
-    @Scheduled(cron = "0 16 11 * * ?")
+    @Scheduled(cron = "0 38 11 * * ?")
     @Cacheable(value = {"halloffame", "strickrank", "timerank"})
     public void comprehensiveUpdate() {
 
