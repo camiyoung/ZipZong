17ae0d51ce85990acb8e9586a1e68b3fd1add096
황승주 <las1260@naver.com>
2022-08-11 18:51:28 +0900

    Fix: cors 스프링에서 열고 nginx 닫기

--- backend/src/main/java/zipzong/zipzong/config/auth/SecurityConfig.java
+++ backend/src/main/java/zipzong/zipzong/config/auth/SecurityConfig.java
@@ -19,8 +19,8 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http
                 .csrf().disable()//csrf 공격을 막아주는 옵션을 disalbe, rest api같은 경우에는 브라우저를 통해 request 받지 않기 때문에 해당 옵션을 꺼도 됩니다.
-                //.cors().configurationSource(corsConfigurationSource())
-                //.and()
+                .cors().configurationSource(corsConfigurationSource())
+                .and()
                 .logout().logoutSuccessUrl("/") //logout 요청시 홈으로 이동 - 기본 logout url = "/logout"
                 .and()
                 .oauth2Login() //OAuth2 로그인 설정 시작점
