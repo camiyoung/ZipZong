a5b1449a097e7a8d86eb9f26a3cf8548b2153f39
황승주 <las1260@naver.com>
2022-08-12 10:54:52 +0900

    Fix: 운동명 수정

--- backend/src/main/java/zipzong/zipzong/enums/Movement.java
+++ backend/src/main/java/zipzong/zipzong/enums/Movement.java
@@ -3,7 +3,7 @@ package zipzong.zipzong.enums;
 import java.util.stream.Stream;
 
 public enum Movement {
-    PUSHUP, BURPEE, MOUNTAINCLIMING, SQUAT, JUMPINGJACK, LUNGE;
+    PUSHUP, BURPEE, SQUAT, JUMPINGJACK, LUNGE, LATERALRAISE;
 
     public static String[] getNames() {
         return Stream.of(Movement.values()).map(Movement::name).toArray(String[]::new);
