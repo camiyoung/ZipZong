04dc0c93d42197554e2c2028e1fa1a1705318c7e
camiyoung <anji6978@naver.com>
2022-08-14 23:49:47 +0900

    Fix: 운동 기록이 없는 경우 종료 메세지만 보이도록 수정

--- frontend/src/pages/ExerciseResutlPage.jsx
+++ frontend/src/pages/ExerciseResutlPage.jsx
@@ -1,10 +1,9 @@
-import React, { useEffect } from "react"
+import React, { useEffect, useState } from "react"
 import Trainer from "../assets/personal_trainer.svg"
 import Workout from "../assets/workout.svg"
 import AOS from "aos"
 import "aos/dist/aos.css"
 import { useSelector } from "react-redux"
-import { getResults } from "../features/room/exerciseReducer"
 
 const ResultCard = ({ name, target, performed }) => {
   return (
@@ -31,73 +30,88 @@ const OtherPlayerResult = ({ name, res }) => {
   )
 }
 export default function ExerciseResulPage() {
-  const result = useSelector(getResults)
+  const [hasRes, setHasRes] = useState(false)
+  const result = useSelector((state) => state.exercise.result)
 
-  const myRes = result.myResult.personalResultDetails
-  let myPercentage =
-    myRes.reduce(
-      (prev, cur) => prev + (cur.performNum / cur.targetNum) * 100,
-      0
-    ) / myRes.length
-
-  console.log(myPercentage)
   useEffect(() => {
+    if (result.myResult && result.allResult) setHasRes(true)
     AOS.init()
   }, [])
+
   return (
-    <div className="w-screen">
-      {/* 운동 종료 안내 섹션  */}
-      <section className=" w-full p-3 h-screen flex flex-col justify-center items-center relative">
-        <img
-          src={Trainer}
-          alt=""
-          className="absolute  -left-32 w-3/6 h-3/6 bottom-0 animate-slide-from-left"
-        />
-        <img
-          src={Workout}
-          alt=""
-          className="absolute  -right-0 w-2/6 h-3/6 top-10  animate-slide-from-right"
-        />
-        <h1 className=" text-4xl font-bold my-4  text-black">
-          운동이 종료되었습니다!
-        </h1>
-        <h2 className="font-semibold text-2xl mb-4">
-          내 달성률 :{myPercentage}%
-        </h2>
-        <h2 className="font-semibold text-2xl mb-12 mt-12">
-          우리팀 평균 달성률 : {result.allResult.avgPercentage}%
-        </h2>
-        <div className="flex w-4/5 justify-center z-30">
-          {result.allResult.personalPercentages.map((user, idx) => (
-            <OtherPlayerResult
-              name={user.nickname}
-              res={user.percentage}
-              key={idx}
+    <>
+      {hasRes ? (
+        <div className="w-screen">
+          {/* 운동 종료 안내 섹션  */}
+          <section className=" w-full p-3 h-screen flex flex-col justify-center items-center relative">
+            <img
+              src={Trainer}
+              alt=""
+              className="absolute  -left-32 w-3/6 h-3/6 bottom-0 animate-slide-from-left"
             />
-          ))}
-        </div>
-        <p className=" text-xl mb-4 absolute bottom-20 animate-pulse ">
-          스크롤을 내려서 기록을 확인해 보세요
-        </p>
-      </section>
-      {/* 운동 루틴 결과 섹션 */}
-      <section className="w-full  flex justify-center items-center flex-col p-3">
-        <h1 className=" text-4xl font-bold my-12 "> 진행한 운동 루틴 </h1>
-        <div className="flex flex-wrap  justify-center">
-          {result.myResult.personalResultDetails.map((item, idx) => (
-            <ResultCard
-              name={item.exerciseName}
-              target={item.targetNum}
-              performed={item.performNum}
-              key={idx}
+            <img
+              src={Workout}
+              alt=""
+              className="absolute  -right-0 w-2/6 h-3/6 top-10  animate-slide-from-right"
             />
-          ))}
+            <h1 className=" text-4xl font-bold my-4  text-black">
+              운동이 종료되었습니다!
+            </h1>
+            <h2 className="font-semibold text-2xl mb-4">
+              내 달성률 :{result.myResult.percentage}%
+            </h2>
+            <h2 className="font-semibold text-2xl mb-12 mt-12">
+              우리팀 평균 달성률 : {result.allResult.avgPercentage}%
+            </h2>
+            <div className="flex w-4/5 justify-center z-30">
+              {result.allResult.personalPercentages.map((user, idx) => (
+                <OtherPlayerResult
+                  name={user.nickname}
+                  res={user.percentage}
+                  key={idx}
+                />
+              ))}
+            </div>
+            <p className=" text-xl mb-4 absolute bottom-20 animate-pulse ">
+              스크롤을 내려서 기록을 확인해 보세요
+            </p>
+          </section>
+          {/* 운동 루틴 결과 섹션 */}
+          <section className="w-full  flex justify-center items-center flex-col p-3">
+            <h1 className=" text-4xl font-bold my-12 "> 진행한 운동 루틴 </h1>
+            <div className="flex flex-wrap  justify-center">
+              {result.myResult.personalResultDetails.map((item, idx) => (
+                <ResultCard
+                  name={item.exerciseName}
+                  target={item.targetNum}
+                  performed={item.performNum}
+                  key={idx}
+                />
+              ))}
+            </div>
+          </section>
+          {/* footer */}
+          <section className="w-full h-40 flex justify-center items-center flex-col p-3"></section>
         </div>
-      </section>
-      {/* footer */}
-      <section className="w-full h-40 flex justify-center items-center flex-col p-3">
-        <div>그룹 페이지로 돌아가기 </div>
-      </section>
-    </div>
+      ) : (
+        <div className="w-screen">
+          <section className=" w-full p-3 h-screen flex flex-col justify-center items-center relative">
+            <img
+              src={Trainer}
+              alt=""
+              className="absolute  -left-32 w-3/6 h-3/6 bottom-0 animate-slide-from-left"
+            />
+            <img
+              src={Workout}
+              alt=""
+              className="absolute  -right-0 w-2/6 h-3/6 top-10  animate-slide-from-right"
+            />
+            <h1 className=" text-4xl font-bold my-4  text-black animate-pulse">
+              운동이 종료되었습니다!
+            </h1>
+          </section>
+        </div>
+      )}
+    </>
   )
 }
