1da522a5998c54815a44a2c040904282791b9c03
songji <youthful_15@naver.com>
2022-08-14 20:33:28 +0900

    Feat: ChangeLanguage 작동 방식 수정

--- frontend/src/features/routine/ChangeLanguage.jsx
+++ frontend/src/features/routine/ChangeLanguage.jsx
@@ -1,28 +1,15 @@
 export default function ChangeLanguage({ exercise, ...restProps }) {
-  const exerciseENG = [
-    "PUSHUP",
-    "BURPEE",
-    "SQUAT",
-    "JUMPINGJACK",
-    "LUNGE",
-    "LATERALRAISE",
-  ]
-  const exerciseKOR = [
-    "팔굽혀펴기",
-    "버피",
-    "스쿼트",
-    "팔벌려뛰기",
-    "런지",
-    "레터럴레이즈",
-  ]
-
+  const exerciseName = {
+    PUSHUP: "팔굽혀펴기",
+    BURPEE: "버피",
+    SQUAT: "스쿼트",
+    JUMPINGJACK: "팔벌려뛰기",
+    LUNGE: "런지",
+    LATERALRAISE: "레터럴레이즈",
+  }
   return (
     <div {...restProps}>
-      {exerciseKOR.map((KOR, index) => {
-        if (exercise === exerciseENG[index]) {
-          return <span key={index}>{KOR}</span>
-        }
-      })}
+      <span>{exerciseName.exercise}</span>
     </div>
   )
 }
