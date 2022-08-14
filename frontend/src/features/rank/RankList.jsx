4bc490aa63c0ffb2e31266ed616722315bce22ad
camiyoung <anji6978@naver.com>
2022-08-14 19:58:20 +0900

    Design : 랭킹페이지 ui 변경

--- frontend/src/features/rank/RankList.jsx
+++ frontend/src/features/rank/RankList.jsx
@@ -16,31 +16,31 @@ const rankList = [
 const ListItem = ({ group }) => {
   return (
     <li
-      className=" w-9/12 flex my-3 bg-white p-4 pl-5 h-16 rounded-3xl shadow-md "
+      className=" w-9/12 flex my-3 bg-white px-2 pl-4 h-16 rounded-3xl shadow-md "
       data-aos="flip-down"
     >
       {group.rank > 0 ? (
         <h2 className="text-black  text-xl  py-2 text-center flex w-full items-center justify-evenly">
-          <div className=" w-2/12   text-primary-300  bg-white  font-semibold  mr-2  ">
+          <div className=" w-1/12   text-primary-300  bg-white  font-semibold  mr-2  ">
             {group.rank}위
           </div>
-          <div className="w-1/6">
+          <div className=" w-2/12  flex justify-center ">
             <Avatar
               img={`/images/badgeIcon/${group.teamIcon}.png`}
               rounded={true}
             />
           </div>
 
-          <div className=" text-center font-semibold text-xl text-gray-500 w-1/6">
+          <div className=" text-center font-semibold text-xl text-gray-500 w-6/12 ">
             {group.teamName}
           </div>
           {group.maxStrick && (
-            <div className=" text-center text-lg  font-medium text-gray-500 w-2/6">
+            <div className=" text-center text-lg  font-medium text-gray-500 w-3/12 ">
               {group.maxStrick} 일째 {group.rank <= 3 && <span>🔥</span>}
             </div>
           )}
           {group.totalTime && (
-            <div className=" text-center text-lg  font-medium text-gray-500 w-2/6">
+            <div className=" text-center text-lg  font-medium text-gray-500 w-3/12 ">
               {group.totalTime} 분째 {group.rank <= 3 && <span>🔥</span>}
             </div>
           )}
@@ -73,7 +73,10 @@ export const RankList = ({
       <h2 className="text-center text-4xl p-2 rounded-3xl m-2  w-4/6  font-bold ">
         {title}
       </h2>
-      <p className=" font-semibold mb-6">{description}</p>
+      <div className="flex flex-col items-center text-gray-500">
+        <p className=" text-md font-medium mb-3">{description[0]}</p>
+      </div>
+
       <div className="w-full">
         <ul className=" flex flex-col justify-center items-center">
           {rankList.map((group, index) => (
--- frontend/src/features/rank/TopRank.jsx
+++ frontend/src/features/rank/TopRank.jsx
@@ -11,12 +11,22 @@ const RankSlider = ({ list }) => {
       rightControl=" "
     >
       {list.map((group, idx) => (
-        <div className="w-full h-full bg-[#ffffff85] text-[#9898cf]" key={idx}>
+        <div
+          className="w-full h-full bg-[#ffffff85] text-[#9898cf] border-double border-4 border-[#d6d6ee] rounded-xl"
+          key={idx}
+        >
           <div className="flex h-full items-center justify-center  text-2xl font-medium">
             {group.rank !== -1 ? (
-              <span>[{group.teamName}] 66일 연속 운동 성공!</span>
+              <p>
+                <span className="underline decoration-solid underline-offset-4  decoration-[#9898cf]  decoration-2 inline-block mx-3">
+                  {group.teamName}
+                </span>
+                <span>
+                  {caculateDate(group.satisfiedTime)}에 66일 연속 운동 달성!
+                </span>
+              </p>
             ) : (
-              <span> 연속 66일의 운동으로 명예의 전당에 도전해보세요 ! </span>
+              <p> 연속 66일의 운동으로 명예의 전당에 도전해보세요 ! </p>
             )}
           </div>
         </div>
@@ -32,14 +42,88 @@ const defaultItem = {
   satisfiedTime: "",
 }
 
+const caculateDate = (day = 0) => {
+  const date = new Date()
+  date.setDate(date.getDate() - day)
+  const completedDay = date
+    .toLocaleDateString()
+    .split(".")
+    .map((item) => item.trim())
+
+  completedDay.pop()
+  const res = `${completedDay[0]}년 ${completedDay[1]}월 ${completedDay[2]}일`
+  return res
+}
+
+const CompletedBadge = ({ day, rank }) => {
+  const color = {
+    1: "moving-grad text-white",
+    2: "bg-pink-600 text-white",
+    3: "bg-yellow-500 text-white",
+  }
+
+  return (
+    <span
+      className={`${color[rank]}  text-sm font-medium inline-flex items-center px-2.5 py-0.5 rounded-lg`}
+    >
+      <svg
+        aria-hidden="true"
+        className="mr-1 w-3 h-3"
+        fill="currentColor"
+        viewBox="0 0 20 20"
+        xmlns="http://www.w3.org/2000/svg"
+      >
+        <path
+          fillRule="evenodd"
+          d="M10 18a8 8 0 100-16 8 8 0 000 16zm1-12a1 1 0 10-2 0v4a1 1 0 00.293.707l2.828 2.829a1 1 0 101.415-1.415L11 9.586V6z"
+          clipRule="evenodd"
+        ></path>
+      </svg>
+      {day === 0 ? (
+        <span>오늘 66일 달성</span>
+      ) : (
+        <span>{day}일전 66일 달성</span>
+      )}
+    </span>
+  )
+}
+
+const Top3Info = ({ team }) => {
+  const gradient = {
+    1: "top1",
+    2: "top2",
+    3: "top3",
+  }
+
+  const { rank, teamName: name, satisfiedTime: day, teamIcon: icon } = team
+  return (
+    <div className=" text-lg font-semibold absolute  flex  flex-col justify-center items-center w-[80%] h-[80%]  ">
+      <div
+        className={` top3 border-double border-4  px-2 rounded-xl flex flex-col items-center py-2  ${gradient[rank]} `}
+      >
+        {rank !== -1 ? (
+          <Avatar img={`/images/badgeIcon/${icon}.png`} />
+        ) : (
+          <Avatar rounded={true} />
+        )}
+        <div className=" px-2 rounded-xl my-1 ">{name}</div>
+        <div className=" font-medium text-base ">
+          {rank !== -1 && <CompletedBadge day={day} rank={rank} />}
+        </div>
+      </div>
+    </div>
+  )
+}
+
 export const TopRank = ({ list }) => {
   const rankList = [...list]
   if (rankList.length < 10) {
-    for (let i = rankList.length; i < 10; i++) rankList.push(defaultItem)
+    for (let i = rankList.length; i < 5; i++) rankList.push(defaultItem)
   }
 
   const top3 = [rankList[0], rankList[1], rankList[2]]
   const under3 = rankList.slice(3)
+
   return (
     <div
       className=" relative  w-full  h-full rounded-3xl flex  items-center justify-center shadow-md"
@@ -52,14 +136,14 @@ export const TopRank = ({ list }) => {
     >
       <div className="  w-full  p-3   flex flex-col items-center justify-center ">
         <div>
-          <h1 className="text-5xl font-extrabold  text-center ">
+          <h1 className="text-5xl font-extrabold  text-center  ml-10 ">
             <span>💎 </span>
             <span className="bg-gradient-to-r from-purple-400 to-pink-600 bg-clip-text text-transparent">
               명예의 전당
             </span>
             <span> 💎</span>
           </h1>
-          <p className="text-center font-semibold mt-4 text-lg">
+          <p className="text-center font-semibold mt-4 text-lg ml-10">
             66일 연속 운동을 달성한 그룹입니다 !
           </p>
         </div>
@@ -70,18 +154,7 @@ export const TopRank = ({ list }) => {
               alt=""
               className=" w-[80%] h-[80%] "
             />
-            <div className="text-lg font-semibold absolute  flex  flex-col justify-center items-center w-[80%] h-[80%]  ">
-              {top3[1].rank !== -1 ? (
-                <Avatar img={`/images/badgeIcon/${top3[1].teamIcon}.png`} />
-              ) : (
-                <Avatar rounded={true} />
-              )}
-
-              <div>{top3[1].teamName} </div>
-              <div className="font-medium text-base ">
-                {top3[1].rank !== -1 && <div>{top3[1].satisfiedTime} 분 </div>}
-              </div>
-            </div>
+            <Top3Info team={top3[1]} />
           </div>
           <div className="w-[33%] h-full flex items-center justify-center animate-prize-gold relative ">
             <img
@@ -89,17 +162,8 @@ export const TopRank = ({ list }) => {
               alt=""
               className=" w-full h-full"
             />
-            <div className="text-lg font-semibold absolute  flex  flex-col justify-center items-center w-[80%] h-[80%]  ">
-              {top3[0].rank !== -1 ? (
-                <Avatar img={`/images/badgeIcon/${top3[0].teamIcon}.png`} />
-              ) : (
-                <Avatar rounded={true} />
-              )}
-
-              <div>{top3[0].teamName} </div>
-              <div className="font-medium text-base ">
-                {top3[0].rank !== -1 && <div>{top3[0].satisfiedTime} 분 </div>}
-              </div>
+            <div className=" text-lg font-semibold absolute  flex  flex-col justify-center items-center w-[80%] h-[80%]  ">
+              <Top3Info team={top3[0]} />
             </div>
           </div>
 
@@ -109,18 +173,7 @@ export const TopRank = ({ list }) => {
               alt=""
               className=" w-[75%] h-[75%]"
             />
-            <div className="text-lg font-semibold absolute  flex  flex-col justify-center items-center w-[80%] h-[80%]  ">
-              {top3[2].rank !== -1 ? (
-                <Avatar img={`/images/badgeIcon/${top3[2].teamIcon}.png`} />
-              ) : (
-                <Avatar rounded={true} />
-              )}
-
-              <div>{top3[2].teamName} </div>
-              <div className="font-medium text-base ">
-                {top3[2].rank !== -1 && <div>{top3[2].satisfiedTime} 분 </div>}
-              </div>
-            </div>
+            <Top3Info team={top3[2]} />
           </div>
         </div>
         <div className=" h-16 w-4/5">
