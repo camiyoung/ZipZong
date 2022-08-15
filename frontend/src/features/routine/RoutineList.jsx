0cf15df5b506a1eaa5ea811cfde32b2d91097fc7
songji <youthful_15@naver.com>
2022-08-15 22:47:20 +0900

    Design: 루틴 페이지 루틴 사이즈 맞춤

--- frontend/src/features/routine/RoutineList.jsx
+++ frontend/src/features/routine/RoutineList.jsx
@@ -36,57 +36,59 @@ export default function RoutineList() {
       <div className="w-full flex">
         {routines.map(({ routineId, routineName, exercise, breakTime }) => {
           return (
-            <div className="w-[20%] m-2" key={routineId}>
-              <div className="p-2 bg-white rounded-2xl border border-gray-200 shadow-md">
-                <div className="w-full">
-                  <div className=" bg-lgBlue-400 shadow-sm m-3 p-1 rounded-2xl flex justify-center font-bold text-xl">
-                    {routineName}
-                  </div>
-                  <div className="flex flex-col justify-center px-3">
-                    {exercise.map(({ name, count }, index) => {
-                      return (
-                        <div
-                          className="flex bg-white py-1 border-b"
-                          key={index}
-                        >
-                          {/* <div className="w-1/6 flex justify-center">
+            <div className="w-[20%]" key={routineId}>
+              <div className="p-2">
+                <div className="p-2 bg-white rounded-2xl border border-gray-200 shadow-md">
+                  <div className="w-full">
+                    <div className=" bg-lgBlue-400 shadow-sm m-3 p-1 rounded-2xl flex justify-center font-bold text-xl">
+                      {routineName}
+                    </div>
+                    <div className="flex flex-col justify-center px-3">
+                      {exercise.map(({ name, count }, index) => {
+                        return (
+                          <div
+                            className="flex bg-white py-1 border-b"
+                            key={index}
+                          >
+                            {/* <div className="w-1/6 flex justify-center">
                           {index + 1}
                         </div> */}
-                          <div className="w-4/6 ml-2 flex justify-start items-center">
-                            <ChangeLanguage exercise={name} />
+                            <div className="w-4/6 ml-2 flex justify-start items-center">
+                              <ChangeLanguage exercise={name} />
+                            </div>
+                            <div className="w-2/6 flex mr-3 justify-end">
+                              {count}회
+                            </div>
                           </div>
-                          <div className="w-2/6 flex mr-3 justify-end">
-                            {count}회
-                          </div>
-                        </div>
-                      )
-                    })}
-                  </div>
-                  <div className="flex justify-center p-3">
-                    쉬는 시간 {breakTime}초
-                  </div>
-                  {/* <div className="flex justify-center">
+                        )
+                      })}
+                    </div>
+                    <div className="flex justify-center p-3">
+                      쉬는 시간 {breakTime}초
+                    </div>
+                    {/* <div className="flex justify-center">
                     총 운동 시간 :{" "}
                     {exercise.length * 60 + (exercise.length - 1) * breakTime}{" "}
                     초
                   </div> */}
-                  <div className="flex justify-center mb-1">
-                    <div
-                      className="bg-secondary-300 m-2 w-[50px] rounded-xl flex justify-center text-sm items-center h-[35px] pointer hover:bg-secondary-500"
-                      onClick={() =>
-                        navigate(`/routine/${params.teamId}/${routineId}`)
-                      }
-                    >
-                      수정
-                    </div>
-                    <div
-                      className="bg-secondary-300 m-2 w-[50px] rounded-xl flex justify-center text-sm items-center pointer hover:bg-secondary-500"
-                      onClick={() => {
-                        setOpen(true)
-                        dispatch(deleteRoutine(routineId))
-                      }}
-                    >
-                      삭제
+                    <div className="flex justify-center mb-1">
+                      <div
+                        className="bg-secondary-300 m-2 w-[50px] rounded-xl flex justify-center text-sm items-center h-[35px] pointer hover:bg-secondary-500"
+                        onClick={() =>
+                          navigate(`/routine/${params.teamId}/${routineId}`)
+                        }
+                      >
+                        수정
+                      </div>
+                      <div
+                        className="bg-secondary-300 m-2 w-[50px] rounded-xl flex justify-center text-sm items-center pointer hover:bg-secondary-500"
+                        onClick={() => {
+                          setOpen(true)
+                          dispatch(deleteRoutine(routineId))
+                        }}
+                      >
+                        삭제
+                      </div>
                     </div>
                   </div>
                 </div>
@@ -96,21 +98,23 @@ export default function RoutineList() {
         })}
 
         {routines.length < 5 ? (
-          <div className="w-[20%] m-2">
-            <div
-              className="p-2 bg-white rounded-2xl border border-gray-200 shadow-md"
-              onClick={() => navigate(`/routine/${params.teamId}/make`)}
-            >
-              <div className="flex justify-center p-2">
-                <ImageIcon
-                  image="https://icons-for-free.com/download-icon-circle+more+plus+icon-1320183136549593898_512.png"
-                  size="large"
-                  borderStyle="none"
-                />
+          <div className="w-[20%]">
+            <div className="p-2">
+              <div
+                className="p-2 bg-white rounded-2xl border border-gray-200 shadow-md"
+                onClick={() => navigate(`/routine/${params.teamId}/make`)}
+              >
+                <div className="flex justify-center p-2">
+                  <ImageIcon
+                    image="https://icons-for-free.com/download-icon-circle+more+plus+icon-1320183136549593898_512.png"
+                    size="large"
+                    borderStyle="none"
+                  />
+                </div>
+                <p className="p-2 font-bold flex justify-center text-lg">
+                  루틴을 추가해보세요.
+                </p>
               </div>
-              <p className="p-2 font-bold flex justify-center text-lg">
-                루틴을 추가해보세요.
-              </p>
             </div>
           </div>
         ) : null}
