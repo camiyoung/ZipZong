4b930b819ea089fe0958d4252e2ec796bcbd36cc
황승주 <las1260@naver.com>
2022-08-17 14:42:58 +0900

    Fix: 토큰 인증 테스트

--- backend/src/main/java/jibjoong/jibjoong/interceptor/AuthInterceptor.java
+++ backend/src/main/java/jibjoong/jibjoong/interceptor/AuthInterceptor.java
@@ -23,6 +23,7 @@ public class AuthInterceptor implements HandlerInterceptor {
         if (jwtService.verifyToken(accessToken)) {
             return true;
         }
+        System.out.println("실행!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
         throw new CustomException(CustomExceptionList.ACCESS_TOKEN_ERROR);
     }
 }
