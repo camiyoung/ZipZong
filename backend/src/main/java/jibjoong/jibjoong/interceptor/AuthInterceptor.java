43cb2de55d95f66896aeea713dd97cd74a75204f
황승주 <las1260@naver.com>
2022-08-17 17:33:50 +0900

    Feat: 인터셉터설정

--- backend/src/main/java/jibjoong/jibjoong/interceptor/AuthInterceptor.java
+++ backend/src/main/java/jibjoong/jibjoong/interceptor/AuthInterceptor.java
@@ -4,6 +4,7 @@ import jibjoong.jibjoong.config.jwt.JwtService;
 import jibjoong.jibjoong.exception.CustomException;
 import jibjoong.jibjoong.exception.CustomExceptionList;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.HandlerInterceptor;
 
@@ -13,12 +14,14 @@ import javax.servlet.http.HttpServletResponse;
 
 @Component
 @RequiredArgsConstructor
+@Slf4j
 public class AuthInterceptor implements HandlerInterceptor {
 
     private final JwtService jwtService;
 
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        log.debug("=======================되나?========================");
         String accessToken = request.getHeader("accessToken");
         if (jwtService.verifyToken(accessToken)) {
             return true;
