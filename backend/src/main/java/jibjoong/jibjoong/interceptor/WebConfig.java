c4e3a453589d6d494990bcc10c60797236219276
황승주 <las1260@naver.com>
2022-08-17 14:21:07 +0900

    Fix: 토큰 인증 적용

--- backend/src/main/java/jibjoong/jibjoong/interceptor/WebConfig.java
+++ backend/src/main/java/jibjoong/jibjoong/interceptor/WebConfig.java
@@ -1,6 +1,7 @@
 package jibjoong.jibjoong.interceptor;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -8,6 +9,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 @RequiredArgsConstructor
 public class WebConfig implements WebMvcConfigurer {
+
     private final AuthInterceptor authInterceptor;
     private final MemberInterceptor memberInterceptor;
     private final TeamInterceptor teamInterceptor;
@@ -16,12 +18,11 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addInterceptors(InterceptorRegistry interceptorRegistry) {
 
-        //
         interceptorRegistry.addInterceptor(authInterceptor)
-                .addPathPatterns("/**")
+                .addPathPatterns("**/**")
                 // 팀 초대 링크로 팀 아이디 조회
-                .excludePathPatterns("/team/invite-link/**",
-                                        "/registration/team/**");
+                .excludePathPatterns("**/team/invite-link/**",
+                                        "**/registration/team/**");
 //        interceptorRegistry.addInterceptor(memberInterceptor)
 //                //.addPathPatterns("/**");
 //                .excludePathPatterns("/**");
