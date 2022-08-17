3a0154827ccdaf06b678c2d888832010536c9418
camiyoung <anji6978@naver.com>
2022-08-18 01:07:40 +0900

    Fix: error

--- frontend/src/pages/RankPage.jsx
+++ frontend/src/pages/RankPage.jsx
@@ -16,8 +16,6 @@ export default function RankPage() {
     audioRef.current.volume = 0.1
     AOS.init()
 
-    console.log(http)
-
     http.get("ranking/info").then((res) => {
       setRankingInfo(res.data.data)
     })
