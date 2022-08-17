7dea398fdb2da4fccb8a2a722a3e75a99ed595c0
박종민 <qooktree@naver.com>
2022-08-17 15:38:32 +0900

    Design: Resize Component 작성 완료

--- frontend/src/pages/NotShow.jsx
+++ frontend/src/pages/NotShow.jsx
@@ -1,7 +1,15 @@
 export default function NotShow() {
   return (
-    <div className="w-full h-[962px] bg-darkGray" style={{ zIndex: "9999" }}>
-      사이즈를 늘려주세요
+    <div
+      className="w-screen h-full bg-white flex justify-center items-center fixed"
+      style={{ position: "absolute", zIndex: "9999", height: "100vh" }}
+    >
+      <div className="flex flex-col items-center">
+        <p className="text-4xl font-semibold">
+          본 사이트는 가로 1240px 이상만 보입니다.
+        </p>
+        <p className="text-4xl mt-10 font-semibold">가로 사이즈를 늘려주세요</p>
+      </div>
     </div>
   )
 }
